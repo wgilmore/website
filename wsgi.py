fc0178d8717e71ae7115dcca1e628e35fec5373c
Matthew Gilmore <wmgilmor@gmail.com>
2020-05-24 19:18:21 -0400

    New look

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,19 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@application.route("/generic")
+def generic():
+    return render_template('generic.html')
+
+@application.route("/elements")
+def elements():
+    return render_template('elements.html')
+
+@application.route("/homepage")
+def homepage():
+    return render_template('homepage.html')
 
 if __name__ == "__main__":
     application.run()
