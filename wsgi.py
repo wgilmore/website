880ea2fdc6fb4041331dc109dff471489856be9c
Matthew Gilmore <wmgilmor@gmail.com>
2020-05-25 17:01:23 -0400

    Updates

--- wsgi.py
+++ wsgi.py
@@ -18,6 +18,9 @@ def blog():
 def about():
     return render_template('about.html')
 
+@application.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     application.run()
